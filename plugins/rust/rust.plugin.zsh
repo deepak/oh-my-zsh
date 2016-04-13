41446d5ee0cf8bb681254224fde3349349fe5e3f
Deepak Kannan <kannan.deepak@gmail.com>
2016-04-13 18:30:55 +0530

    Add alias for rustc --explain to rust plugin

--- plugins/rust/rust.plugin.zsh
+++ plugins/rust/rust.plugin.zsh
@@ -3,9 +3,22 @@
 # https://github.com/robbyrussell/oh-my-zsh/pull/3007
 # and https://github.com/rust-lang/zsh-config/blob/master/_rust
 
+# aliases for cargo
 alias cb="cargo build"
 alias cbr="cargo build --release"
 alias ct="cargo test"
+alias cr="cargo run"
+
+# aliases for rustc
+function rce() {
+    if [[ -z $@ ]]; then
+        echo "Usage: $0:t <Error-Message-Code>"
+        echo "Example: rce E0425"
+        return 1
+    else
+        rustc --explain "$@" | less
+    fi
+}
 
 function _cargo_commands() {
     local ret=1 state
